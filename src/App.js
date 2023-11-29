e528b1e7a06ed58bd7075c02ca6ba0cb83dc76bc
Ariela Israel <arielaisrael12@gmail.com>
2023-11-29 01:55:45 -0500

    lesson complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
-import UserProfile from "./UserProfile";
+// import UserProfile from "./UserProfile";
 import NoMatch from "./NoMatch";
 import BackButton from "./BackButton";
 import ForwardButton from "./ForwardButton";
 import GoHomeButton from "./GoHomeButton";
+// import PostList from './PostList';
+import User from './User';
 import "./App.css";
 
 function App() {
@@ -25,7 +27,7 @@ function App() {
           <About />
         </Route>
         <Route path="/users/:userId">
-          <UserProfile />
+          <User />
         </Route>
         <Route>
           <NoMatch />
